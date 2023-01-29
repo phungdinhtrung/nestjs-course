3e79f2853db6bfbb2430a2f4176bfaa6bd12fda2
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2023-01-29 13:40:21 +0700

    Merge branchs

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Res, Param, Post, Patch, Delete, Body, HttpStatus } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService){}
+
+  // /coffees
+  @Get()
+  findAll(@Res() res) {
+    res.status(HttpStatus.OK).json(this.coffeesService.findAll());
+  }
+
+  // /coffees/:id
+  @Get(':id')
+  findOne(@Param('id') id: number, @Res() res) {   
+    res.status(HttpStatus.OK).json(this.coffeesService.findOne(id));
+  }
+
+  // coffees
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto, @Res() res){
+    res.status(HttpStatus.OK).json(this.coffeesService.create(createCoffeeDto));
+  }
+
+
+  // coffees/:id
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto, @Res() res) {
+    res.status(HttpStatus.OK).json(this.coffeesService.update(+id, updateCoffeeDto));
+  }
+
+  // coffees/:id
+  @Delete(':id')
+  delete(@Param('id') id: string, @Res() res) {
+    res.status(HttpStatus.OK).json(this.coffeesService.delete(+id));
+  }
+}
+
---
+++ src/coffees/dto/create-coffee.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNumber } from 'class-validator'
+
+export class CreateCoffeeDto {
+    @IsNumber()
+    readonly id: number;
+
+    @IsString()
+    readonly name: string;
+
+    @IsString()
+    readonly brand: string;
+
+    @IsString({each: true})
+    readonly flavors: string[];
+}
